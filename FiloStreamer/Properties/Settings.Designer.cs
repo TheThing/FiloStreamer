0aca333c0e218d9949752413059e14cf080e73ab
Jonatan Nilsson <jonatan@nilsson.is>
2015-07-22 07:50:26 +0000

    Added folder settings for recordings

--- FiloStreamer/Properties/Settings.Designer.cs
+++ FiloStreamer/Properties/Settings.Designer.cs
@@ -238,5 +238,17 @@ namespace FiloStreamer.Properties {
                 this["settingsAdvanced"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string settingsRecordingFolder {
+            get {
+                return ((string)(this["settingsRecordingFolder"]));
+            }
+            set {
+                this["settingsRecordingFolder"] = value;
+            }
+        }
     }
 }
--- FiloStreamer/Recorder/LocalRecorder.cs
+++ FiloStreamer/Recorder/LocalRecorder.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using FiloStreamer.Properties;
 using FiloStreamer.Encoder;
 using System.Windows.Threading;
@@ -13,6 +14,7 @@ namespace FiloStreamer.Recorder
     class LocalRecorder : BasicEncoder
     {
         private bool _isRecording;
+        private string _file;
 
         public LocalRecorder(Dispatcher dispatcher)
             : base(dispatcher, "Rec")
@@ -25,7 +27,16 @@ namespace FiloStreamer.Recorder
             string video = string.Format("-vcodec copy");
             string audio = string.Format("-acodec copy");
             string network = string.Format("-i udp://{0}", Settings.Default.networkLocal);
-            string arguments = string.Format("-y -threads 4 {0} -map 0 {1} {2} \"test.mp4\"", network, video, audio);
+            var time = DateTime.Now;
+            File = Path.Combine(Properties.Settings.Default.settingsRecordingFolder,
+                                       string.Format("{0}-{1:00}-{2:00}_{3:00}-{4:00}-{5:00}.mp4",
+                                                     time.Year,
+                                                     time.Month,
+                                                     time.Day,
+                                                     time.Hour,
+                                                     time.Minute,
+                                                     time.Second));
+            string arguments = string.Format("-y -threads 4 {0} -map 0 {1} {2} \"{3}\"", network, video, audio, File);
             this.IsRecording = true;
             base.Run(arguments);
         }
@@ -33,12 +44,23 @@ namespace FiloStreamer.Recorder
         public void Stop()
         {
             _process.Stop();
+            File = "";
         }
 
         protected override void _process_ProcessExited(object sender, EventArgs e)
         {
             base._process_ProcessExited(sender, e);
             this.IsRecording = false;
+        }
+
+        public string File
+        {
+            get { return _file; }
+            set
+            {
+                _file = value;
+                RunPropertyChanged("File");
+            }
         }
 
         public bool IsRecording
